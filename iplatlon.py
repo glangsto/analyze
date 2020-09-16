41a1649ad07357c58d7f25f3019777e1883fd18a
Glen Langston <glen.i.langston@gmail.com>
2020-09-16 07:08:50 -0400

    show lat,lon in degrees, minutes, seconds

--- iplatlon.py
+++ iplatlon.py
@@ -14,10 +14,50 @@ country=data['country']
 region=data['region']
 loc = data['loc']
 locs = loc.split(',')
-lat = float( locs[0])
-lon = float( locs[1])
+flat = float( locs[0])
+flon = float( locs[1])
 
-print lat,lon
-
-print 'Your IP detail\n '
+print 'Your IP details\n '
 print 'IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP)
+
+# keep latitude and longitude in float format
+lat = flat
+lon = flon
+
+# convert to degrees minutes seconds
+# get sign of latitude
+if lat > 0:
+    pmlat = '+'
+else:
+    pmlat = '-'
+    lat = - lat
+
+# get sign of longitude
+if lon > 0:
+    pmlon = '+'
+else:
+    pmlon = '-'
+    lon = - lon
+
+# get degrees part
+dlat = int(lat)
+dlon = int(lon)
+
+# start on minutes part
+lat = (lat - dlat)*60.
+lon = (lon - dlon)*60.
+
+mlat = int(lat)
+mlon = int(lon)
+
+# start on seconds part
+slat = (lat - mlat)*60.
+slon = (lon - mlon)*60.
+
+# show results
+print('Latitude : %9.5f' % (flat))
+print('Latitude : %s%02d:%02d:%05.2f' % (pmlat,dlat,mlat,slat))
+print('Longitude: %9.5f' % (flon))
+print('Longitude: %s%02d:%02d:%05.2f' % (pmlon,dlon,mlon,slon))
+
+
