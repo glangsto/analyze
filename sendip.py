0bd0f64446dc7ac175e5b1aba0bfd2f70a9afd13
Glen Langston <glen.i.langston@gmail.com>
2020-11-10 09:19:12 -0500

    Email web ip on command line

--- sendip.py
+++ sendip.py
@@ -1,16 +1,13 @@
 # python to get my ip and send an email to megajansky
 # HISTORY
+# 20Nov10 GIL try to readh web ip
 # 20Nov03 GIL initial version
 
 import os
 
 myiptemp = "/tmp/myip"
 os.system( "/home/pi/bin/myip | head -1 > %s" % (myiptemp))
-
-# prepare for failure to read parts of ip
-ipparts = [ "1", "2"]
-
-# read all the ip addresses
+    # read all the ip addresses
 with open(myiptemp, "r") as f:
     for line in f:
         ipparts = str(line)
@@ -19,18 +16,36 @@ f.close()
 
 parts = ipparts.split('\n')
 myip = parts[0]
-#print ("My IP: %s" % (myip))
-# 
-ipparts = "/tmp/myip." + myip
 
-parts = ipparts.split('\n')
-ipfile = parts[0]
+webip = "127.0.0.1"
+webiptemp = "/tmp/webip"
+
+# prepare for failure to read parts of ip
+ipparts = [ "1", "2"]
+
+try:
+    # read all the ip addresses
+    with open(webiptemp, "r") as f:
+        for line in f:
+            ipparts = str(line)
+
+    f.close()
+
+    if len(ipparts) > 0:
+        parts = ipparts.split('\n')
+        webip = parts[0]
+    
+except:
+    webip = myip
+    
+print ("My IP: %s, Web IP: %s" % (myip, webip))
+
 #now write ip to a temp file
 mailtemp = "/tmp/myemail"
 os.system( "echo 'FROM: root' > %s" % (mailtemp))
 os.system( "echo 'TO: root' >> %s" % (mailtemp))
-os.system( "echo 'Subject: Pi Booting: %s' >> %s" % (myip,mailtemp))
-os.system( "echo 'Log of Pis booting up: %s' >> %s" % (myip, mailtemp))
+os.system( "echo 'Subject: Pi Booting: %s' >> %s" % (webip,mailtemp))
+os.system( "echo 'Log of Pis booting up: %s' >> %s" % (webip, mailtemp))
 
 # read all the user-suppied horn info into the mail log
 os.system( "cat /boot/horn.txt >> %s" % (mailtemp))
@@ -45,4 +60,5 @@ os.system( "cat %s | /usr/sbin/sendmail -t" % (mailtemp))
 
 # now clean up for next go
 os.system( "sudo rm -f %s" % (myiptemp))
+#os.system( "sudo rm -f %s" % (webiptemp))
 os.system( "sudo rm -f %s" % (mailtemp))
