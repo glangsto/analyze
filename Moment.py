20acf184188cc523d03756b56a439e230a9e2b16
glangsto <glen.i.langston@gmail.com>
2024-10-15 07:38:26 -0400

    Python 2->3

--- Moment.py
+++ Moment.py
@@ -2,6 +2,7 @@
 # variance, skew and kurtosis
 # grid with a convolving function
 # HISTORY
+# 23Oct23 GIL python 2->3
 # 17JAN28 GIL finish initial version
 
 import numpy as np
@@ -23,7 +24,7 @@ def moment(  xs, ys):
     curt = 0
     sum = 0
     if (n <= 2):
-        print "Array length n must be at least 2 in moment"
+        print("Array length n must be at least 2 in moment")
         return (ave,adev,sdev,var,skew,curt)
 
 # First pass to get the mean.
@@ -46,7 +47,7 @@ def moment(  xs, ys):
 
 #Put the pieces together according to the conventional definitions.
     adev = adev / n
-    var  = (var-(ep*ep)/n)/(n-1); 
+    var  = (var-(ep*ep)/n)/(n-1);
     sdev = np.sqrt(var);
     if var > 0:
         skew = skew / (n * var * sdev)
@@ -61,21 +62,21 @@ def test_main():
     """
     xs = np.linspace(-10, 10, 20)
     ys = np.linspace(-10, 10, 20)
-    ys[5] = 0.    
+    ys[5] = 0.
     ys[15] = 0.
 
     # test the code
     (ave,adev,sdev,var,skew,curt) = moment(xs, ys)
     # display the inputs
 
-    print 'ys[0-4]  : ',ys[0:5]
-    print 'ys[5-9]  : ',ys[5:10]
-    print 'ys[10-14]: ',ys[10:15]
-    print 'ys[15-19]: ',ys[15:20]
+    print('ys[0-4]  : ',ys[0:5])
+    print('ys[5-9]  : ',ys[5:10])
+    print('ys[10-14]: ',ys[10:15])
+    print('ys[15-19]: ',ys[15:20])
 
     # display the outputs
-    print 'ave,adev,sdev: ', ave,adev,sdev
-    print 'var,skew,curt: ', var,skew,curt
+    print('ave,adev,sdev: ', ave,adev,sdev)
+    print('var,skew,curt: ', var,skew,curt)
 
 if __name__ == "__main__":
     """
@@ -107,5 +108,3 @@ if __name__ == "__main__":
 #ventional definitions.
 #*curt=(*curt)/(n*(*var)*(*var))-3.0;
 #} else nrerror("No skew/kurtosis when variance = 0 (in moment)");
-
-
